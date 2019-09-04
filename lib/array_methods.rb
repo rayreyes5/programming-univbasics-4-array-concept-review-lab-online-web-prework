205483639c9f8a49ffe225b1b8a80fb060e68e69
Ray Reyes <rayreyes5@gmail.com>
2019-09-04 02:23:25 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,9 @@
 def find_element_index(array, value_to_find)
- 
+ number.times do
 end
 
 def find_max_value(array)
- (find_max_value(scale)).to eq(5)
+  
 end
 
 def find_min_value(array)
- (find_min_value(scale)).to eq(2)
-end
